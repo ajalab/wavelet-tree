d59df880918093cde974f1afb87e724816a4e264
Koki Kato <koki.kato1994@gmail.com>
2019-04-30 19:57:46 +0900

    Use num-traits

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,26 @@
 use std::fmt;
 
 use fid::{BitVector, FID};
-use try_from::TryFrom;
+use num_traits::Num;
+use std::ops::{BitOr, Shl};
 
-pub struct WaveletMatrix {
+pub struct WaveletMatrix<T> {
     rows: Vec<BitVector>,
-    size: usize,
+    size: u64,
     len: u64,
-    partitions: Vec<usize>,
+    partitions: Vec<u64>,
+    _t: std::marker::PhantomData<T>,
 }
 
-impl WaveletMatrix {
-    pub fn new_with_size<T: Into<u64> + Copy + Clone, K: AsRef<[T]>>(text: K, size: usize) -> Self {
+impl<T> WaveletMatrix<T>
+where
+    T: Into<u64> + Copy + Clone + Num + BitOr<T, Output = T> + Shl<u64, Output = T>,
+{
+    pub fn new_with_size<K: AsRef<[T]>>(text: K, size: u64) -> Self {
         let mut rows: Vec<BitVector> = vec![];
         let mut zeros: Vec<T> = text.as_ref().to_vec();
         let mut ones: Vec<T> = Vec::new();
-        let mut partitions: Vec<usize> = Vec::new();
+        let mut partitions: Vec<u64> = Vec::new();
         for r in 0..size {
             let mut bv = BitVector::new();
             let mut new_zeros: Vec<T> = Vec::new();
@@ -35,77 +40,75 @@ impl WaveletMatrix {
             zeros = new_zeros;
             ones = new_ones;
             rows.push(bv);
-            partitions.push(zeros.len());
+            partitions.push(zeros.len() as u64);
         }
         WaveletMatrix {
             rows: rows,
             size: size,
             len: text.as_ref().len() as u64,
             partitions: partitions,
+            _t: std::marker::PhantomData::<T>,
         }
     }
 
-    pub fn new<T: Into<u64> + Copy + Clone, K: AsRef<[T]>>(text: K) -> Self {
-        Self::new_with_size(text, std::mem::size_of::<T>() * 8)
+    pub fn new<K: AsRef<[T]>>(text: K) -> Self {
+        Self::new_with_size(text, std::mem::size_of::<T>() as u64 * 8)
     }
 
-    pub fn access<T: TryFrom<usize> + Copy + Clone>(&self, k: usize) -> T
-    where
-        <T as try_from::TryFrom<usize>>::Err: std::fmt::Debug,
-    {
-        let mut i = k as u64;
-        let mut n = 0usize;
+    pub fn access(&self, k: u64) -> T {
+        let mut i = k;
+        let mut n = T::zero();
         for (r, bv) in self.rows.iter().enumerate() {
             let b = bv.get(i);
             if b {
-                i = self.partitions[r] as u64 + bv.rank1(i);
-                n |= 1 << (self.size - r - 1);
+                i = self.partitions[r] + bv.rank1(i);
+                n = n | (T::one() << (self.size - (r as u64) - 1));
             } else {
                 i = bv.rank0(i);
             }
         }
-        TryFrom::try_from(n).unwrap()
+        n
     }
 
-    pub fn rank<T: Into<u64> + Copy + Clone>(&self, c: T, k: u64) -> usize {
+    pub fn rank(&self, c: T, k: u64) -> u64 {
         let n = c.into();
         let mut s = 0u64;
         let mut e = if k < self.len { k } else { self.len };
         for (r, bv) in self.rows.iter().enumerate() {
-            let b = (n >> (self.size - r - 1)) & 1 > 0;
+            let b = (n >> (self.size - (r as u64) - 1)) & 1 > 0;
             s = bv.rank(b, s);
             e = bv.rank(b, e);
             if b {
-                let z = self.partitions[r] as u64;
+                let z = self.partitions[r];
                 s = s + z;
                 e = e + z;
             }
         }
-        (e - s) as usize
+        e - s
     }
 
-    pub fn select<T: Into<usize> + Copy + Clone>(&self, c: T, k: usize) -> usize {
+    pub fn select(&self, c: T, k: u64) -> u64 {
         let n = c.into();
         let mut s = 0u64;
         for (r, bv) in self.rows.iter().enumerate() {
-            let b = (n >> (self.size - r - 1)) & 1 > 0;
+            let b = (n >> (self.size - (r as u64) - 1)) & 1 > 0;
             s = bv.rank(b, s);
             if b {
-                let z = self.partitions[r] as u64;
+                let z = self.partitions[r];
                 s = s + z;
             }
         }
-        let mut e = s + (k as u64);
+        let mut e = s + k;
         for (r, bv) in self.rows.iter().enumerate().rev() {
-            let b = (n >> (self.size - r - 1)) & 1 > 0;
+            let b = (n >> (self.size - (r as u64) - 1)) & 1 > 0;
             if b {
-                let z = self.partitions[r] as u64;
+                let z = self.partitions[r];
                 e = bv.select1(e - z);
             } else {
                 e = bv.select0(e);
             }
         }
-        e as usize
+        e
     }
 
     pub fn len(&self) -> u64 {
@@ -113,7 +116,7 @@ impl WaveletMatrix {
     }
 }
 
-impl fmt::Debug for WaveletMatrix {
+impl<T: fmt::Debug> fmt::Debug for WaveletMatrix<T> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         let len = self.rows[0].len();
         writeln!(f, "WaveletMatrix {{")?;
@@ -161,7 +164,7 @@ mod tests {
         let wm = WaveletMatrix::new_with_size(numbers, size);
         assert_eq!(wm.len, numbers.len() as u64);
         for (i, &n) in numbers.iter().enumerate() {
-            assert!(wm.access::<u8>(i) == n, "wm.access({}) == {}", i, n);
+            assert!(wm.access(i as u64) == n, "wm.access({}) == {}", i, n);
         }
     }
 
@@ -171,14 +174,20 @@ mod tests {
         let size = 3;
         let wm = WaveletMatrix::new_with_size(numbers, size);
 
-        let mut ans: Vec<Vec<usize>> = vec![vec![]; 1 << size];
+        let mut ans: Vec<Vec<u64>> = vec![vec![]; 1 << size];
         for (i, &n) in numbers.iter().enumerate() {
-            ans[n as usize].push(i);
+            ans[n as usize].push(i as u64);
         }
 
         for (c, a) in ans.iter().enumerate() {
             for (k, &i) in a.iter().enumerate() {
-                assert!(wm.select(c, k) == i, "wm.select({}, {}) == {}", c, k, i);
+                assert!(
+                    wm.select(c as u8, k as u64) == i,
+                    "wm.select({}, {}) == {}",
+                    c,
+                    k,
+                    i
+                );
             }
         }
     }
